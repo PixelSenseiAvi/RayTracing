7c5be6708cbcff44fc31ef963ade10cfac3133a8
chikvi <a7vi1912@gmail.com>
2022-12-03 23:46:37 +0530

    v2

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -2,6 +2,20 @@
 
 #include "Walnut/Random.h"
 
+
+namespace Utils 
+{
+	static uint32_t ConvertToRGBA(glm::vec4 color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0);
+		uint8_t g = (uint8_t)(color.g * 255.0);
+		uint8_t b = (uint8_t)(color.b * 255.0);
+		uint8_t a = (uint8_t)(color.a * 255.0);
+
+		return ((a << 24) | (b << 16) | (g << 8) | r);
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -26,7 +40,10 @@ void Renderer::Render()
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
 			coord = coord * 2.0f - 1.0f;
-			m_ImageData[x + y*m_FinalImage->GetWidth()] = PerPixel(coord);
+
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 
 		}
 	}
@@ -37,7 +54,7 @@ void Renderer::Render()
 
 
 //Fragment Shader
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
 
 	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
@@ -55,9 +72,23 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	//D = b^2 -4ac
 	float discriminant = b * b - 4.0f * a * c;
 
-	if (discriminant >= 0.0f)
-		return 0xffff00ff;
+	if (discriminant < 0.0f)
+		return glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
 
-	return 0xff000000;
+	// (-b +- sqrt(D))/2a
+	float t0 = (-b + glm::sqrt(discriminant)) / 2.0f * a;
+	float t1 = (-b - glm::sqrt(discriminant)) / 2.0f * a;
+
+	//glm::vec3 h0 = rayOrigin + rayDirection * t0;
+	glm::vec3 h1 = rayOrigin + rayDirection * t1; //closet hit point as t1 will be smallest
+	glm::vec3 normal = glm::normalize(h1);
+
+	glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
+
+	float d = glm::max(glm::dot(normal, -lightDir), 0.0f);
+
+	glm::vec3 sphereColor(1.0f, 0.0f, 1.0f);
+	sphereColor *= d;
+	return glm::vec4(sphereColor, 1.0f);
 }
 
